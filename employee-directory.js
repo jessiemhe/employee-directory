2f9557e574ac98b683007f9de3b9e2e36474b7b8
Jessie He <hejessie98@gmail.com>
2018-02-21 10:39:40 -0500

    Update employee-directory.js

--- employee-directory.js
+++ employee-directory.js
@@ -1,44 +1,50 @@
-var info = {};
+var click = document.getElementById("card1");
+click.addEventListener("click", function(){
+    
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://raw.githubusercontent.com/thejesshe/employee-directory/master/package.json') 
 ourRequest.onload = function(){
     ourData = JSON.parse(ourRequest.responseText);
-    info = ourData;
+    alert("Name: " +
+          ourData[0].name +
+          "\n" + "Position: " +
+          ourData[0].position +
+          "\n" + "Extension: " +
+          ourData[0].extension);
 }
-ourRequest.send();
-
-var click = document.getElementById("card1");
-click.addEventListener("click", function(){
-    var myGen = function*() {
-        var one = yield info[0];
-        var two = yield "done";
-    }
-    
-    var gen = myGen();
-    console.log(gen.next());
-    console.log(gen.next());
+ourRequest.send(); 
 });
 
 var click = document.getElementById("card2");
 click.addEventListener("click", function(){
-    var myGen = function*() {
-        var one = yield info[1];
-        var two = yield "done";
-    }
     
-    var gen = myGen();
-    console.log(gen.next());
-    console.log(gen.next());
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://raw.githubusercontent.com/thejesshe/employee-directory/master/package.json') 
+ourRequest.onload = function(){
+    ourData = JSON.parse(ourRequest.responseText);
+    alert("Name: " +
+          ourData[1].name +
+          "\n" + "Position: " +
+          ourData[1].position +
+          "\n" + "Extension: " +
+          ourData[1].extension);
+}
+ourRequest.send(); 
 });
 
 var click = document.getElementById("card3");
 click.addEventListener("click", function(){
-    var myGen = function*() {
-        var one = yield info[2];
-        var two = yield "done";
-    }
     
-    var gen = myGen();
-    console.log(gen.next());
-    console.log(gen.next());
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET','https://raw.githubusercontent.com/thejesshe/employee-directory/master/package.json') 
+ourRequest.onload = function(){
+    ourData = JSON.parse(ourRequest.responseText);
+    alert("Name: " +
+          ourData[2].name +
+          "\n" + "Position: " +
+          ourData[2].position +
+          "\n" + "Extension: " +
+          ourData[2].extension);
+}
+ourRequest.send(); 
 });
